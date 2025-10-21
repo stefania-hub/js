42e554b3c1636fb2116f5efc3b77184d0c2a364c
stefania-hub <stefaniadagnino@yahoo.it>
2025-10-21 13:31:39 +0200

    esercitazione js

---
+++ control-flow.js
@@ -0,0 +1,7 @@
+const age= prompt (Quanti anni hai?)
+const ageNumber = Number(age)
+if (ageNumber===0){
+    alert ("Sei un boomer?")
+} else if (ageNumber <15){
+    alert ()
+}
---
+++ script.js
@@ -0,0 +1,98 @@
+// console.log("helloworld");
+
+//boolean
+true;
+false;
+
+//number
+3;
+3-14;
+-34,4;
+Infinity;
+-Infinity;
+
+NaN;
+
+//string
+"ciao mondo"
+"a"
+""
+
+//tipi nulli
+undefined
+null
+
+//OPERATORI LOGICI
+
+//NOT
+true;//vera
+!true;//falso
+
+
+//console.log(!false)
+false;//falsa
+!false;//vero
+
+//AND
+true && true; //true
+true && false; //false
+false && true; //false
+false && false; //false
+
+//OR
+true || true; //true
+false || false; //false
+false || true; //true
+
+
+//OPERATORI MATEMATICI
+
+//+
+5 + 10; //15
+//-
+90 -10; //80
+//*
+6 * 8; //48
+///
+9 / 3; //3
+//** 
+2**3 //8
+4**2//16
+
+// somma di stringhe(+)
+"ciao" + "mamma" //"ciaomamma"
+"ciao" + "" + "mamma" //"ciao mamma"
+
+//OPERATORI DI CONFRONTO
+//uguaglianza (== o ===)
+5 === 5; //true
+7 === 2; //false
+
+//diversità (!= o !==)
+5 !== 3//true
+5 !== 5//false
+!(7 === 7)//false
+
+//maggiore e maggiore o uguale (> o >=)
+// minore e minore o uguale (< o <=)
+
+// parentesi (())
+//variabile
+var firstNumber = 5;
+
+//LET
+
+//CONST
+
+const qui = true
+const quo = false
+const qua = qui && quo;
+console.log(qua);
+
+//Type of
+const variabile1 = 3
+const variabile2 = true
+const variabile3 = "axia"
+
+
+
