6d7e221368515a2f58278e748d3ff13b2832f139
stefania-hub <stefaniadagnino@yahoo.it>
2025-10-22 08:38:05 +0200

    compiti a casa

--- control-flow.js
+++ control-flow.js
@@ -3,5 +3,19 @@ const ageNumber = Number(age)
 if (ageNumber===0){
     alert ("Sei un boomer?")
 } else if (ageNumber <15){
-    alert ()
+    alert ("Complimenti, non sei un boomer!")
+}  if ( ageNumber >55){
+    alert ("Sei un boomer!")
+}
+
+
+
+
+
+let nomeUtente = prompt("Come ti chiami?");
+
+if (nomeUtente && nomeUtente () !== "") {
+  alert("Auguri, " + nomeUtente + "!");
+} else {
+  alert("Non hai inserito il nome. Auguri lo stesso!");
 }
