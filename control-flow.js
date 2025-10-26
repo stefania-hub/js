4d83b2409284b85b8d0792e7bda0c27342501760
stefania-hub <stefaniadagnino@yahoo.it>
2025-10-26 13:04:28 +0100

    compiti eloquent js

--- control-flow.js
+++ control-flow.js
@@ -52,4 +52,43 @@
 // }
 // console.log("sono uscito dal ciclo for")
 
+//ESERCIZIO UNO
+let line = "";
+for (let i = 0; i < 7; i++) {
+  line += "#";
+  console.log(line);
+}
 
+
+//ESERCIZIO DUE
+for (let n = 1; n <= 100; n++) {
+  if (n % 15 === 0) {
+    console.log("FizzBuzz");
+  } else if (n % 3 === 0) {
+    console.log("Fizz");
+  } else if (n % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(n);
+  }
+}
+
+//ESERCIZIO TRE
+function chessboard(size) {
+  let board = "";
+  for (let r = 0; r < size; r++) {
+    let line = "";
+    for (let c = 0; c < size; c++) {
+      if ((r + c) % 2 === 0) {
+        line += " ";
+      } else {
+        line += "#";
+      }
+    }
+    board += line + "\n";
+  }
+  return board;
+}
+
+// Esempio: stampa scacchiera 8x8
+console.log(chessboard(8));
