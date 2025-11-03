7419cf994ee7e127134e0001495e0f59e4363e47
stefania-hub <stefaniadagnino@yahoo.it>
2025-11-03 13:26:01 +0100

    array

---
+++ array.js
@@ -0,0 +1,9 @@
+console.log('Array')
+let numbers = [ 20, 18, 22, 44, 35, 0, -1, 1,5, 100, 20];
+console.log(numbers.lenght); //-> 10;
+console.log(numbers[3]);
+
+let bouleans = [true, true, true, false, true];
+
+
+let strings = 
